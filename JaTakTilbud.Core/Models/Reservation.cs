5198428247791625e414b49b942e14a30bfaed67
Jforby4 <jforby4@gmail.com>
2026-04-15 10:34:15 +0200

    Modellag
    Alle klasser i modellaget er Færdig
    -User
    -Reservation

--- JaTakTilbud.Core/Models/Reservation.cs
+++ JaTakTilbud.Core/Models/Reservation.cs
@@ -8,5 +8,24 @@ namespace JaTakTilbud.Core.Models
 {
     internal class Reservation
     {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public int CampaignProductId { get; set; }
+
+        public int CollectingNumber { get; set; }
+
+        public DateTime ReservedAt { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string Status { get; set; }
+    }
+    public enum Status
+    {
+        Confirmed = 0,
+        Cancelled = 1,
+        Expired = 2,
     }
 }
